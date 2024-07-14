8cb8d4f20bf6dcb02694452798f0bf8e9a0c113d
Muhammadiy <163087296+MuhammaDIY125@users.noreply.github.com>
2024-07-14 13:40:58 +0500

    update

--- main.py
+++ main.py
@@ -12,26 +12,30 @@ st.dataframe(df)
 st.write("## Diagramma")
 st.write("### Yillar bo'yicha dunyoda chekadiganlar soni")
 
+tab1, tab2, tab3 = st.tabs(["Umumiy", "Jins bo'yicha", "Yoshlar"])
 
-
-tab1, tab2, tab3, tab4 = st.tabs(["Umumiy", "Ayollar", "Erkaklar", "Ayollar va Erkaklar"])
-
-def chiqar(y, tpl=('', '')):
+with tab1:
     fig, ax = plt.subplots()
-    sns.lineplot(x='Year', y=y[0], data=df, ax=ax, label=tpl[0])
-    sns.lineplot(x='Year', y=y[-1], data=df, ax=ax, label=tpl[-1])
+    sns.lineplot(x='Year', y='Smoking_Population_Percentage', data=df, ax=ax)
     plt.xlabel('Yil')
     plt.ylabel('Chekadiganlar %')
     st.pyplot(fig)
 
-with tab1:
-    chiqar(('Smoking_Population_Percentage',))
 with tab2:
-    chiqar(('Male_Smokers_Percentage',))
+    fig, ax = plt.subplots()
+    sns.lineplot(x='Year', y='Male_Smokers_Percentage', data=df, ax=ax, label='Erkak')
+    sns.lineplot(x='Year', y='Female_Smokers_Percentage', data=df, ax=ax, label='Ayol')
+    plt.xlabel('Yil')
+    plt.ylabel('Chekadiganlar %')
+    st.pyplot(fig)
+
 with tab3:
-    chiqar(('Female_Smokers_Percentage',))
-with tab4:
-    chiqar(('Male_Smokers_Percentage','Female_Smokers_Percentage'), ('Erkak', 'Ayol'))
+    fig, ax = plt.subplots()
+    sns.barplot(data=df, x='Year', y='Smoking_Population_Percentage', ax=ax, label='Umumiy', order=[i*10+1930 for i in range(10)])
+    sns.barplot(data=df, x='Year', y='Youth_Smokers_Percentage', ax=ax, label='Yoshlar', order=[i*10+1930 for i in range(10)])
+    plt.xlabel('Yil')
+    plt.ylabel('Chekadiganlar %')
+    st.pyplot(fig)
 
 """
 Year,
