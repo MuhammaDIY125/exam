a95dbabf4c7007541f67636380463fcd1385be47
Muhammadiy <163087296+MuhammaDIY125@users.noreply.github.com>
2024-07-14 09:34:57 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.write("#Assalomu alaykum")
+
+df = pd.read_csv('world_smoking_history_1924_2023.csv')
+
+df
